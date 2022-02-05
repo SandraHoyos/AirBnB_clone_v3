aa6603974ff233dab46e7ae86c778243d87f4e47
Sandra Calero <3358@holbertonschool.com>
2022-02-04 22:49:21 -0500

    fix striict slashes and move content from /api/v1/__init__.py to /api/v1/views/__init__.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,14 +6,15 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(self):
     """Method that calls storage.close()"""
     storage.close()
+
 
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 
-app_views.url_map.strict_slashes = False
-@app_views.route('/status')
+
+@app_views.route('/status', strict_slashes=False)
 def status():
     """Returns a JSON: "status": "OK"
     """
