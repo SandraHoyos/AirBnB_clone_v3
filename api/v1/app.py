a57123bf463bf081c34d66d883129e6fa8eb459a
Sandra Hoyos <slhoyosr@gmail.com>
2022-02-04 23:34:37 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,21 @@
 """
 from os import getenv
 from flask import Flask
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Handler for 404 errors that returns a JSON-formatted
+       404 status code response
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
