3491462d23338ec4e0ee6452decde982abfc2b64
Sandra Calero <3358@holbertonschool.com>
2022-02-06 16:44:51 -0500

    fix docstring of the module

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""View for State objects that handles all default RESTFul API actions"""
+"""View for Amenity objects that handles all default RESTFul API actions"""
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage
--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""City objects that handles all default RESTFul API actions"""
+"""View for City objects that handles all default RESTFul API actions"""
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage
